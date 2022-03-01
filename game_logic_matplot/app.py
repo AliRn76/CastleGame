951cdad927e039a62bf013fbbb338c9c1eccdc34
AliRn76 <alirn76@yahoo.com>
2022-03-02 00:25:00 +0330

    Fix find enemy in other lines

--- game_logic_matplot/app.py
+++ game_logic_matplot/app.py
@@ -28,6 +28,7 @@ class Game:
         print()
 
         cls.print_units()
+        sleep(.6)
 
     @staticmethod
     def units():
@@ -69,13 +70,15 @@ class Unit(Game):
     def has_target(self):
         return bool(self._target)
 
-    def get_attacked(self, damage):
+    def get_attacked(self, damage) -> bool:
         if self._hp <= damage:
             print(f'{self.name} Just Died.')
             self._hp = 0
             self.died()
+            return True
         else:
             self._hp -= damage
+            return False
 
     def died(self):
         self.is_dead = True
@@ -85,10 +88,9 @@ class Unit(Game):
         self.remove_point(self._x, self._y, self._z)
 
     def attack(self):
-        if self._target.is_dead:
+        target_died = self._target.get_attacked(self._attack)
+        if target_died:
             self._target = None
-        else:
-            self._target.get_attacked(self._attack)
 
     def is_won(self):
         if self._team:
@@ -105,14 +107,61 @@ class Unit(Game):
         self.remove_point(self._x, self._y, self._z)
         self._x, self._y, self._z = self.set_unit(self._x + self._speed, self._y, self._z)
 
+    def _check_index(self, *_point):
+        x, y, z = _point
+        len_z = len(board)
+        len_y = len(board[0])
+        len_x = len(board[0][0])
+        # print(f'{(self._y + y) = } ')
+        # sleep(1)
+        if 0 <= self._z + z < len_z \
+                and 0 <= self._y + y < len_y \
+                and 0 <= self._x + x < len_x:
+            return True
+        else:
+            return False
+
     def is_enemy_in_range(self) -> bool:
-        __range = range(1, self._range) if self._range > 0 else range(self._range, -1)
+        __range = range(1, self._range) if self._range > 0 else range(self._range, 0)
         for i in __range:
-            if board[self._z][self._y][self._x + i] is not EMPTY:
-                self.set_target(board[self._z][self._y][self._x + i]['id'])
-                return True
-            else:
-                return False
+            # print(f'Range: {i}')
+            if self._check_index(i, 0, 0):  # Check Forward
+                # print(f'Forward \t\t {board[self._z][self._y][self._x + i]}')
+                # sleep(1)
+                if board[self._z][self._y][self._x + i] is not EMPTY:
+                    # print('into Forward')
+                    # sleep(1)
+                    self.set_target(board[self._z][self._y][self._x + i]['id'])
+                    return True
+
+            if self._check_index(0, abs(i), 0):  # Check Down
+                # print(f'Down \t\t {board[self._z][self._y + abs(i)][self._x]}')
+                # sleep(1)
+                if board[self._z][self._y + abs(i)][self._x] is not EMPTY:
+                    # print('into Down')
+                    # sleep(1)
+                    self.set_target(board[self._z][self._y + abs(i)][self._x]['id'])
+                    return True
+
+            if self._check_index(0, -abs(i), 0):  # Check Up
+                # print(f'Up \t\t {board[self._z][self._y - abs(i)][self._x]}')
+                # sleep(1)
+                if board[self._z][self._y - abs(i)][self._x] is not EMPTY:
+                    # print('into Up')
+                    # sleep(1)
+                    self.set_target(board[self._z][self._y - abs(i)][self._x]['id'])
+                    return True
+
+            if self._check_index(-i, 0, 0):  # Check Back
+                # print(f'Back \t\t {board[self._z][self._y][self._x - i]}')
+                # sleep(1)
+                if board[self._z][self._y][self._x - i] is not EMPTY:
+                    # print('into Back')
+                    # sleep(1)
+                    self.set_target(board[self._z][self._y][self._x - i]['id'])
+                    return True
+
+        return False
 
     def set_unit(self, *_point) -> tuple:
         x, y, z = _point
@@ -121,34 +170,42 @@ class Unit(Game):
         return _point
 
 
-u1 = Unit(x=0, y=1, z=0, hp=100, attack=20, range=2, speed=1, is_team1=True)
-database.append({'id': u1.name, 'obj': u1})
-u2 = Unit(x=0, y=1, z=0, hp=100, attack=10, range=2, speed=1, is_team1=False)
-database.append({'id': u2.name, 'obj': u2})
-# u3 = Unit(x=0, y=2, z=0, hp=100, attack=20, range=2, speed=1, is_team1=False)
-# database.append({'id': u3.name, 'obj': u3})
-
-for i, d in enumerate(database):
-    print(f'Player {i}: {d["id"]}')
+def create_units():
+    u1 = Unit(x=0, y=0, z=0, hp=100, attack=20, range=2, speed=1, is_team1=True)
+    database.append({'id': u1.name, 'obj': u1})
+    u2 = Unit(x=0, y=1, z=0, hp=100, attack=10, range=2, speed=1, is_team1=False)
+    database.append({'id': u2.name, 'obj': u2})
+    # u3 = Unit(x=0, y=2, z=0, hp=100, attack=20, range=2, speed=1, is_team1=False)
+    # database.append({'id': u3.name, 'obj': u3})
 
 
-nobody_wins = True
-while nobody_wins:
-    Game.print_ground()
-    sleep(.6)
-    for unit in Game.units():
-        if unit.is_dead:
-            del unit
-        else:
-            if unit.is_won():
-                print(f'Player {unit.name} Just Won The Game.')
-                nobody_wins = False
-                break
+def run():
+    nobody_wins = True
+    while nobody_wins:
+        # Game.print_ground()
+        _units = Game.units()
+        for u in _units:
 
-            if unit.has_target() or unit.is_enemy_in_range():
-                unit.attack()
+            Game.print_ground()
+            print(f'\n-----{u._team} --> {u.name}')
+            if u.is_dead:
+                del u
             else:
-                unit.move_straight()
+                if u.is_won():
+                    print(f'Player {u.name} Just Won The Game.')
+                    nobody_wins = False
+                    break
+
+                if u.has_target() or u.is_enemy_in_range():
+                    u.attack()
+                else:
+                    u.move_straight()
+
+
+if __name__ == '__main__':
+    create_units()
+    run()
+
 
 # TODO:
 # [x]   1. Create Board
@@ -157,7 +214,7 @@ while nobody_wins:
 # [x]   4. Move Unit Till Find First Enemy in Line
 # [x]   5. Stop & Attack Enemy
 # [x]   6. Move After Killing Enemy
-# [ ]   7. Find Enemy In Other Lines
+# [x]   7. Find Enemy In Other Lines
 # [ ]   8. Set Action Time For Attack
 # [ ]   9. Find Measure For Speed And Range (board x = 1000, speed = 2, range = 50
 # [ ]   10. Change Line When It Couldn't Move In His Line (randomly left or right)
@@ -167,4 +224,4 @@ while nobody_wins:
 # [ ]   14. User Wins After Destroyed Tower
 # [ ]   15. Add Air Units
 # [ ]   16. Find Air Enemies
-# [ ]   17. Attack Of Air Cards 
+# [ ]   17. Attack Of Air Cards
