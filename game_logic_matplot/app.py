8be5b70bbcc0ca3406effd7248f4cbce4cf95c28
AliRn76 <alirn76@yahoo.com>
2022-03-04 22:10:29 +0330

    Add debug logs for unit range + kinda fix parallel movement and attack + fix some bug

--- game_logic_matplot/app.py
+++ game_logic_matplot/app.py
@@ -1,4 +1,8 @@
+import asyncio
 import os
+import time
+from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
 from time import sleep
 
 
@@ -6,7 +10,7 @@ RANGE_X = 10
 RANGE_Y = 3
 RANGE_Z = 3
 EMPTY = '-'
-
+DEBUG_RANGE = False
 board = [[[EMPTY for _ in range(RANGE_X)] for _ in range(RANGE_Y)] for _ in range(RANGE_Z)]
 database = []
 
@@ -18,6 +22,8 @@ class Game:
 
     @classmethod
     def print_ground(cls):
+        if DEBUG_RANGE:
+            sleep(2)
         os.system('clear')  # on Linux System
 
         ground = board[0]
@@ -41,7 +47,8 @@ class Game:
             print(f'Unit: {u.name}\t\t HP: {u.hp}')
 
     @classmethod
-    def remove_point(cls, *_point):
+    def _remove_point(cls, *_point):
+        """ Set EMPTY to given point """
         x, y, z = _point
         board[z][y][x] = EMPTY
         return _point
@@ -60,7 +67,7 @@ class Unit(Game):
         self._team = is_team1
         self._target = None
         self.is_dead = False
-        self.set_unit(self._x, self._y, self._z)
+        self._set_unit(self._x, self._y, self._z)
         self.name = id(self)
 
     @property
@@ -72,7 +79,6 @@ class Unit(Game):
 
     def get_attacked(self, damage) -> bool:
         if self._hp <= damage:
-            print(f'{self.name} Just Died.')
             self._hp = 0
             self.died()
             return True
@@ -81,13 +87,17 @@ class Unit(Game):
             return False
 
     def died(self):
+        print(f'{self.name} Just Died.')
+        sleep(3)
         self.is_dead = True
         for i in database:
             if i['id'] == self.name:
-                del i
-        self.remove_point(self._x, self._y, self._z)
+                database.remove(i)
+        self._remove_point(self._x, self._y, self._z)
 
     def attack(self):
+        # print("\nFunction attack() is running at time: " + str(int(time.time())) + " seconds.")
+        # sleep(1)
         target_died = self._target.get_attacked(self._attack)
         if target_died:
             self._target = None
@@ -98,22 +108,24 @@ class Unit(Game):
         else:
             return bool(self._x == 0)
 
-    def set_target(self, _id):
+    def set_target(self, _id) -> None:
+        """ get Object from DB and Set it to self._target """
         for data in database:
             if data['id'] == _id:
                 self._target = data['obj']
 
     def move_straight(self) -> None:
-        self.remove_point(self._x, self._y, self._z)
-        self._x, self._y, self._z = self.set_unit(self._x + self._speed, self._y, self._z)
+        """ Clean current position & set new position for unit with self._set_unit() """
+        self._remove_point(self._x, self._y, self._z)
+        self._x, self._y, self._z = self._set_unit(self._x + self._speed, self._y, self._z)
 
-    def _check_index(self, *_point):
+    def _check_index(self, *_point) -> bool:
+        """ return True if the index is exists (not IndexError) else False"""
         x, y, z = _point
         len_z = len(board)
         len_y = len(board[0])
         len_x = len(board[0][0])
-        # print(f'{(self._y + y) = } ')
-        # sleep(1)
+
         if 0 <= self._z + z < len_z \
                 and 0 <= self._y + y < len_y \
                 and 0 <= self._x + x < len_x:
@@ -122,48 +134,49 @@ class Unit(Game):
             return False
 
     def is_enemy_in_range(self) -> bool:
-        __range = range(1, self._range) if self._range > 0 else range(self._range, 0)
+        __range = range(1, self._range + 1, 1) if self._team else range(-1, self._range - 1, -1)
+
+        debug_wait_time = 1
+        if DEBUG_RANGE:
+            print()
+
         for i in __range:
-            # print(f'Range: {i}')
             if self._check_index(i, 0, 0):  # Check Forward
-                # print(f'Forward \t\t {board[self._z][self._y][self._x + i]}')
-                # sleep(1)
+                if DEBUG_RANGE:
+                    print(f'Forward\t {"-->" if self._team else "<--"} \t [Range: {abs(i)}]\t {board[self._z][self._y][self._x + i]}')
+                    sleep(debug_wait_time)
                 if board[self._z][self._y][self._x + i] is not EMPTY:
-                    # print('into Forward')
-                    # sleep(1)
                     self.set_target(board[self._z][self._y][self._x + i]['id'])
                     return True
 
             if self._check_index(0, abs(i), 0):  # Check Down
-                # print(f'Down \t\t {board[self._z][self._y + abs(i)][self._x]}')
-                # sleep(1)
+                if DEBUG_RANGE:
+                    print(f'Down \t {"-->" if self._team else "<--"} \t [Range: {abs(i)}]\t {board[self._z][self._y + abs(i)][self._x]}')
+                    sleep(debug_wait_time)
                 if board[self._z][self._y + abs(i)][self._x] is not EMPTY:
-                    # print('into Down')
-                    # sleep(1)
                     self.set_target(board[self._z][self._y + abs(i)][self._x]['id'])
                     return True
 
             if self._check_index(0, -abs(i), 0):  # Check Up
-                # print(f'Up \t\t {board[self._z][self._y - abs(i)][self._x]}')
-                # sleep(1)
+                if DEBUG_RANGE:
+                    print(f'Up \t {"-->" if self._team else "<--"} \t [Range: {abs(i)}]\t {board[self._z][self._y - abs(i)][self._x]}')
+                    sleep(debug_wait_time)
                 if board[self._z][self._y - abs(i)][self._x] is not EMPTY:
-                    # print('into Up')
-                    # sleep(1)
                     self.set_target(board[self._z][self._y - abs(i)][self._x]['id'])
                     return True
 
             if self._check_index(-i, 0, 0):  # Check Back
-                # print(f'Back \t\t {board[self._z][self._y][self._x - i]}')
-                # sleep(1)
+                if DEBUG_RANGE:
+                    print(f'Back \t {"-->" if self._team else "<--"} \t [Range: {abs(i)}]\t {board[self._z][self._y][self._x - i]}')
+                    sleep(debug_wait_time)
                 if board[self._z][self._y][self._x - i] is not EMPTY:
-                    # print('into Back')
-                    # sleep(1)
                     self.set_target(board[self._z][self._y][self._x - i]['id'])
                     return True
 
         return False
 
-    def set_unit(self, *_point) -> tuple:
+    def _set_unit(self, *_point) -> tuple:
+        """ Set new position in board with given point """
         x, y, z = _point
         _data = {'character': self._character, 'hp': self._hp, 'id': id(self)}
         board[z][y][x] = _data
@@ -173,39 +186,65 @@ class Unit(Game):
 def create_units():
     u1 = Unit(x=0, y=0, z=0, hp=100, attack=20, range=2, speed=1, is_team1=True)
     database.append({'id': u1.name, 'obj': u1})
-    u2 = Unit(x=0, y=1, z=0, hp=100, attack=10, range=2, speed=1, is_team1=False)
+
+    u2 = Unit(x=0, y=1, z=0, hp=100, attack=20, range=2, speed=2, is_team1=False)
     database.append({'id': u2.name, 'obj': u2})
+
     # u3 = Unit(x=0, y=2, z=0, hp=100, attack=20, range=2, speed=1, is_team1=False)
     # database.append({'id': u3.name, 'obj': u3})
 
 
-def run():
-    nobody_wins = True
+nobody_wins = True
+
+def main():
+    global nobody_wins
+
     while nobody_wins:
-        # Game.print_ground()
+        Game.print_ground()
         _units = Game.units()
-        for u in _units:
 
-            Game.print_ground()
-            print(f'\n-----{u._team} --> {u.name}')
-            if u.is_dead:
-                del u
-            else:
-                if u.is_won():
-                    print(f'Player {u.name} Just Won The Game.')
-                    nobody_wins = False
-                    break
+        if len(_units) == 0:
+            print('Draw')
+            break
 
-                if u.has_target() or u.is_enemy_in_range():
-                    u.attack()
-                else:
-                    u.move_straight()
+        # # # Model 1
+        # for u in _units:
+        #     run(u)
+
+        # # # Model 2
+        # with ThreadPoolExecutor(max_workers=len(_units)) as e:
+        #     for u in _units:
+        #         e.submit(run, u)
+
+        # # # Model 3
+        tasks = [Thread(target=run, args=(u, )) for u in _units]
+        [t.start() for t in tasks]
+        # [t.join() for t in tasks]
+
+        # # # Model 4
+        # tasks.append(asyncio.create_task(run(u)))
+
+def run(u: Unit) -> None:
+    global nobody_wins
+
+    if u.is_dead:
+        del u
+    else:
+        if u.is_won():
+            print(f'Player {u.name} Just Won The Game.')
+            nobody_wins = False
+        elif u.has_target() or u.is_enemy_in_range():
+            # sleep(.1)  # Don't know why, but it won't work correct without this sleep.
+            u.attack()
+        else:
+            sleep(.1)  # Don't know why, but it won't work correct without this sleep.
+            u.move_straight()
 
 
 if __name__ == '__main__':
     create_units()
-    run()
-
+    main()
+    # asyncio.run(main())
 
 # TODO:
 # [x]   1. Create Board
@@ -215,6 +254,9 @@ if __name__ == '__main__':
 # [x]   5. Stop & Attack Enemy
 # [x]   6. Move After Killing Enemy
 # [x]   7. Find Enemy In Other Lines
+# [x]   7.1. Fix Range
+# [x]   7.2. Make It Parallel
+# [ ]   7.3 Change The "is not EMPTY" In is_enemy_in_range() TO Something Like: "is unit"
 # [ ]   8. Set Action Time For Attack
 # [ ]   9. Find Measure For Speed And Range (board x = 1000, speed = 2, range = 50
 # [ ]   10. Change Line When It Couldn't Move In His Line (randomly left or right)
